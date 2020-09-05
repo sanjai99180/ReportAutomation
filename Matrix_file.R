a8eceed16eb7e45ba7558e3f0bb0671a7f09a548
Sanjai Kumar <sanjai99180@gmail.com>
2020-09-05 15:30:22 +0530

    added

--- Matrix_file.R
+++ Matrix_file.R
@@ -30,14 +30,14 @@ for (i in 0:(8 - 1)) {
 }
 #==============read file======
 library(xlsx)
-data=read.xlsx("C:/Users/SANJAI/Desktop/Matrix_file.xlsx", sheetIndex = 1, check.names=F) 
+data=read.xlsx("C:/Users/SANJAI/Desktop/Module_Exercise/Interview_ExcerciseFile/Matrix_file.xlsx", sheetIndex = 1, check.names=F) 
 for (i in 1:nrow(data)){
   for(j in 1:(ncol(data)-1)) data[i,j+1]=paste(j,LETTERS[i],"-Matrix", sep = '')
 }
 
 #============Score_Description========
 library(xlsx)
-data=read.xlsx('C:/Users/SANJAI/Desktop/Module_Exercise/ExcerciseFile/Score_Description.xlsx', sheetIndex = 1, check.names=F)
+data=read.xlsx('C:/Users/SANJAI/Desktop/Module_Exercise/Interview_ExcerciseFile/Score_Description.xlsx', sheetIndex = 1, check.names=F)
 data$Score=paste(data$Score*100, '%', sep = '')
 data$ScoreDescription=paste(data$`Employee Name`,data$Score)
 
@@ -47,7 +47,7 @@ data$ScoreDescription=paste(data$`Employee Name`,data$Score)
 library(xlsx)
 library(stringr)
 library(dplyr)
-data1=read.xlsx("C:/Users/SANJAI/Desktop/Module_Exercise/ExcerciseFile/Short_Name.xlsx", sheetIndex = 1, check.names=F)
+data1=read.xlsx("C:/Users/SANJAI/Desktop/Module_Exercise/Interview_ExcerciseFile/Short_Name.xlsx", sheetIndex = 1, check.names=F)
 
 data1$`Executive Name`=trimws(data1$`Executive Name`)
 data1$`Short Name`=paste(paste(substring(data1$`Executive Name`,1,1),substring(stringr::word(data1$`Executive Name`,2),1,1), sep=""), stringr::word(data1$`Executive Name`,3))
@@ -55,12 +55,12 @@ data1$`Short Name`=paste(paste(substring(data1$`Executive Name`,1,1),substring(s
 
 #==================Extract DOB==============
 library(stringr)
-df1=read.xlsx("C:/Users/SANJAI/Desktop/Module_Exercise/ExcerciseFile/DOB.xlsx", sheetIndex = 1, check.names=F)
+df1=read.xlsx("C:/Users/SANJAI/Desktop/Module_Exercise/Interview_ExcerciseFile/DOB.xlsx", sheetIndex = 1, check.names=F)
 df1$DOB=str_extract(df1$`When is your date of birth?`, "[0-9/]{8,}")
 
 #=======================Extract Name and Emp_id===================
 library(stringr)
-df2=read.xlsx("C:/Users/SANJAI/Desktop/Module_Exercise/ExcerciseFile/Extract_Emp_id_and_Name.xlsx",sheetIndex = 1, check.names=F)
+df2=read.xlsx("C:/Users/SANJAI/Desktop/Module_Exercise/Interview_ExcerciseFile/Extract_Emp_id_and_Name.xlsx",sheetIndex = 1, check.names=F)
 df2$`Employee Details`=trimws(df2$`Employee Details`) 
 # str="A C Prithviraj (A10718)"
 # substring(str,1,regexpr("[(]",str)[[1]][1]-2)
